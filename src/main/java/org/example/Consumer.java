b6b5a4de3ce67c1d992cb9deb4c4e0f335a4b8ac
Thomas Iffland <tiffland@thinkport.digital>
2023-09-19 11:12:57 +0200

    initial commit

---
+++ src/main/java/org/example/Consumer.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-first-consumer-group");
+        properties.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "consumer1");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(List.of("my-first-topic"));
+
+        while (true) {
+            ConsumerRecords<String, String> results = kafkaConsumer.poll(Duration.of(100, ChronoUnit.MILLIS));
+            for (ConsumerRecord<String, String> result : results) {
+                System.out.printf("Message received in topic %s and partition %s, key was %s, value was %s %n", result.topic(), result.partition(), result.key(), result.value());
+            }
+        }
+
+
+
+    }
+}
---
+++ src/main/java/org/example/Producer.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Producer {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        kafkaProducer.send(new ProducerRecord<>("my-first-topic","Hello World"));
+
+        kafkaProducer.flush();
+    }
+}
